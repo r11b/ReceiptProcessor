e83abcd104989051fcbb0cb3dc4cc743048433a9
r11b <rushika.bhattad11@gmail.com>
2025-01-17 14:01:26 -0800

    Modified validations

--- src/main/java/com/assignment/receipt_points/ReceiptController.java
+++ src/main/java/com/assignment/receipt_points/ReceiptController.java
@@ -16,6 +16,9 @@ public class ReceiptController {
 	
 	private final ReceiptPointsCalculator receiptPointsCalculator;
 	
+	private final String ReceiptNotFoundResponseMessage = "No receipt found for that ID.";
+	private final String InvalidReceiptResponseMessage = "The receipt is invalid.";
+	
 	public ReceiptController(ReceiptPointsCalculator receiptPointsCalculator)
 	{
 		this.receiptPointsCalculator = receiptPointsCalculator;
@@ -24,7 +27,10 @@ public class ReceiptController {
 	@PostMapping("/process")
 	public ResponseEntity<?> processReceipt(@RequestBody Receipt receipt)
 	{
-		this.validateReceipt(receipt);
+		if (!this.isReceiptValid(receipt))
+		{
+			return ResponseEntity.status(400).body(Map.of("error", this.InvalidReceiptResponseMessage));
+		}
 		
 		String receiptId = this.receiptPointsCalculator.processReceipt(receipt);
 		return ResponseEntity.ok(Map.of("id", receiptId));
@@ -34,51 +40,68 @@ public class ReceiptController {
     public ResponseEntity<?> getPoints(@PathVariable String id) {
         Long points = this.receiptPointsCalculator.getPoints(id);
         if (points == null) {
-            return ResponseEntity.status(404).body(Map.of("error", "No receipt found for that ID."));
+            return ResponseEntity.status(404).body(Map.of("error", this.ReceiptNotFoundResponseMessage));
         }
         
         return ResponseEntity.ok(Map.of("points", points));
     }
 	
-	private void validateReceipt(Receipt receipt) {
+	private boolean isReceiptValid(Receipt receipt) {
 	    if (receipt == null) {
-	        throw new IllegalArgumentException("Receipt cannot be null.");
+	        return false;
 	    }
 	    
 	    if (receipt.getRetailer() == null || receipt.getRetailer().isEmpty()) {
-	        throw new IllegalArgumentException("Retailer name is missing.");
+	    	return false;
 	    }
 	    
 	    if (!receipt.getRetailer().matches("^[\\w\\s\\-&]+$") ) {
-	        throw new IllegalArgumentException("Retailer name is in invalid format.");
+	    	return false;
 	    }
 	    
 	    if (receipt.getTotal() == null || receipt.getTotal().isEmpty()) {
-	        throw new IllegalArgumentException("Total is missing.");
+	    	return false;
 	    }
 	    
 	    if (!receipt.getTotal().matches("^\\d+\\.\\d{2}$")) { 
-	        throw new IllegalArgumentException("Total is in an invalid format.");
+	    	return false;
 	    }
 	    
 	    if (receipt.getItemList() == null || receipt.getItemList().isEmpty()) {
-	        throw new IllegalArgumentException("Item list cannot be empty.");
+	    	return false;
 	    }
+	    for(int i = 0; i < receipt.getItemList().size(); i++)
+		{
+			Item item = receipt.getItemList().get(i);
+			if (item.getShortDescription() == null || item.getShortDescription().trim().isEmpty()) {
+		        return false;
+		    }
+			if(!item.getPrice().matches("^\\d+\\.\\d{2}$"))
+			{
+				return false;
+			}
+		    double priceValue = Double.parseDouble(item.getPrice());
+		    if (priceValue < 0) {
+		        return false;
+		    }
+		}
 	    
-	    if (receipt.getPurchaseDate() == null || receipt.getPurchaseDate().isEmpty()) { //^\d{4}-\d{2}-\d{2}$
-	        throw new IllegalArgumentException("Purchase date is missing.");
+	    if (receipt.getPurchaseDate() == null || receipt.getPurchaseDate().isEmpty()) { 
+	    	return false;
 	    }
 	    
 	    if (!receipt.getPurchaseDate().matches("^\\d{4}-\\d{2}-\\d{2}$")) {
-	        throw new IllegalArgumentException("Purchase date is in an invalid format.");
+	    	return false;
 	    }
 	    
 	    if (receipt.getPurchaseTime() == null || receipt.getPurchaseTime().isEmpty()) {
-	        throw new IllegalArgumentException("Purchase time is missing.");
+	    	return false;
 	    }
 	    
 	    if (!receipt.getPurchaseTime().matches("^(?:[01]\\d|2[0-3]):[0-5]\\d$") ) {
-	        throw new IllegalArgumentException("Purchase time is in an invalid format.");
+	        return false;
 	    }
+	    
+	    return true;
 	}
 }
--- src/main/java/com/assignment/receipt_points/ReceiptPointsCalculator.java
+++ src/main/java/com/assignment/receipt_points/ReceiptPointsCalculator.java
@@ -31,128 +31,76 @@ public class ReceiptPointsCalculator {
 	
 	public long calculatePoints(Receipt receipt)
 	{
-		try
-		{
-			String retailer = receipt.getRetailer();
-			String total = receipt.getTotal();
-			List<Item> itemList = receipt.getItemList();
+
+		String retailer = receipt.getRetailer();
+		String total = receipt.getTotal();
+		List<Item> itemList = receipt.getItemList();
 			
-			long points = 0;
-			
-			// Rule 1 : One point for every alphanumeric character in the retailer name. 
-			for(int i = 0; i < retailer.length(); i++) {
-				char ch = retailer.charAt(i);
-				if(Character.isLetterOrDigit(ch))
-				{
-					points++;
-				}
-			}
-			
-			// Rule 2 : 50 points if the total is a round dollar amount with no cents.
-			boolean match = total.matches("^\\d+\\.00$");
-			if (match)
+		long points = 0;
+		
+		// Rule 1 : One point for every alphanumeric character in the retailer name. 
+		for(int i = 0; i < retailer.length(); i++) {
+			char ch = retailer.charAt(i);
+			if(Character.isLetterOrDigit(ch))
 			{
-				points += 50;
+				points++;
 			}
-			
-			// Rule 3 : 25 points if the total is a multiple of 0.25.
-			try {
-				double totalValue = Double.parseDouble(total);
-				if (totalValue < 0) {
-			        throw new IllegalArgumentException("Total cannot be negative.");
-			    }
-				
-				if(totalValue % 0.25 == 0)
-				{
-					points += 25;
-				}
-			} catch (NumberFormatException e) {
-			    throw new IllegalArgumentException("Invalid total format. Must be a numeric value.");
-			}
-			
-			// Rule 4 : 5 points for every two items on the receipt.
-			int itemCount = itemList.size();
-			int pairs = itemCount/2;
-			points += pairs * 5;
-			
-			
-			// Rule 5 : If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer.
-			// The result is the number of points earned.
-			for(int i = 0; i < itemCount; i++)
+		}
+		
+		// Rule 2 : 50 points if the total is a round dollar amount with no cents.
+		boolean match = total.matches("^\\d+\\.00$");
+		if (match)
+		{
+			points += 50;
+		}
+		
+		// Rule 3 : 25 points if the total is a multiple of 0.25.
+		double totalValue = Double.parseDouble(total);
+		
+		if(totalValue % 0.25 == 0)
+		{
+			points += 25;
+		}
+		
+		// Rule 4 : 5 points for every two items on the receipt.
+		int itemCount = itemList.size();
+		int pairs = itemCount/2;
+		points += pairs * 5;
+		
+		
+		// Rule 5 : If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer.
+		// The result is the number of points earned.
+		for(int i = 0; i < itemCount; i++)
+		{
+			Item item = itemList.get(i);
+			String trimShortDescription = item.getShortDescription().trim();
+			if(trimShortDescription.length() % 3 == 0)
 			{
-				Item item = itemList.get(i);
-				if (item.getShortDescription() == null || item.getShortDescription().trim().isEmpty()) {
-			        throw new IllegalArgumentException("Item description cannot be empty.");
-			    }
-			    try {
-			        double priceValue = Double.parseDouble(item.getPrice());
-			        if (priceValue < 0) {
-			            throw new IllegalArgumentException("Item price cannot be negative.");
-			        }
-			    } catch (NumberFormatException e) {
-			        throw new IllegalArgumentException("Invalid item price format for: " + item.getShortDescription());
-			    }
-			    
-				String trimShortDescription = item.getShortDescription().trim();
-				if(trimShortDescription.length() % 3 == 0)
-				{
-					points += (int)Math.ceil(Double.parseDouble(item.getPrice()) * 0.2);
-				}
+				points += (int)Math.ceil(Double.parseDouble(item.getPrice()) * 0.2);
 			}
-			
-			// Rule 6 : 6 points if the day in the purchase date is odd.
-			
-			try {
-				String purchaseDate = receipt.getPurchaseDate();
-				String[] dateParts = purchaseDate.split("-");
-				if (dateParts.length != 3) {
-			        throw new IllegalArgumentException("Invalid purchase date format. Must be YYYY-MM-DD.");
-			    }
-			    
-			    int month = Integer.parseInt(dateParts[1]);
-			    int day = Integer.parseInt(dateParts[2]);
-	
-			    if (month < 1 || month > 12 || day < 1 || day > 31) {
-			        throw new IllegalArgumentException("Invalid purchase date values.");
-			    }
-			    if(day % 2 == 1)
-				{
-					points += 6;
-				}
-			} catch (NumberFormatException e) {
-			    throw new IllegalArgumentException("Invalid purchase date format. Must contain numeric values.");
-			}
-			
-			// Rule 7 : 10 points if the time of purchase is after 2:00pm and before 4:00pm.
-			try {
-				String purchaseTime = receipt.getPurchaseTime();
-			    String[] timeParts = purchaseTime.split(":");
-			    if (timeParts.length != 2) {
-			        throw new IllegalArgumentException("Invalid purchase time format. Must be HH:mm.");
-			    }
-			    int hour = Integer.parseInt(timeParts[0]);
-			    int minute = Integer.parseInt(timeParts[1]);
-	
-			    if (hour < 0 || hour > 23 || minute < 0 || minute > 59) {
-			        throw new IllegalArgumentException("Invalid purchase time values.");
-			    }
-			    if(hour == 14 || hour == 15)
-				{
-					points += 10;
-				}
-			} catch (NumberFormatException e) {
-			    throw new IllegalArgumentException("Invalid purchase time format. Must contain numeric values.");
-			}
-			
-			return points;
 		}
-		catch(IllegalArgumentException e)
+		
+		// Rule 6 : 6 points if the day in the purchase date is odd.
+		
+		String purchaseDate = receipt.getPurchaseDate();
+		String[] dateParts = purchaseDate.split("-");
+	    
+	    int day = Integer.parseInt(dateParts[2]);
+	    if(day % 2 == 1)
 		{
-			throw e;
+			points += 6;
 		}
-		catch (Exception e) 
+		
+		// Rule 7 : 10 points if the time of purchase is after 2:00pm and before 4:00pm.
+		String purchaseTime = receipt.getPurchaseTime();
+	    String[] timeParts = purchaseTime.split(":");
+		int hour = Integer.parseInt(timeParts[0]);
+	    
+		if(hour == 14 || hour == 15)
 		{
-	        throw new RuntimeException("An unexpected error occurred while calculating points: " + e.getMessage(), e);
-	    }
+			points += 10;
+		}
+		
+		return points;
 	}
 }
